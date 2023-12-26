df6920828022722b255668986289f3ced9b1ed0a
Vivek7d <vivekdhalkari07@gmail.com>
2023-12-26 17:59:03 +0530

    last-4 commit

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -11,20 +11,28 @@ interface Props {
 }
 function Banner({ movies }: Props) {
   const [movie, setMovie] = useState<Movie | null>(null);
-  useEffect(() => {
-    setMovie(movies[Math.floor(Math.random() * movies.length)]);
-  }, [movies]);
-  
+
+useEffect(() => {
+  setMovie(movies[Math.floor(Math.random() * movies.length)]);
+}, [movies]);
+let imagepath=''
+// Conditionally log the movie only if it is not null
+if (movie !== null) {
+  imagepath=movie?.backdrop_path || movie?.poster_path
+  console.log(movie);
+}
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12 ">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+      {imagepath && (
         <Image
-          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          src={`${baseUrl}${imagepath}`}
           alt="netflixOriginal"
           fill
-          style={{objectFit:"cover"}}
+          style={{ objectFit: "cover" }}
           priority={true}
         />
+      )}
       </div>
 
       <h1 className="text-2xl font-bold md:text-4xl lg:text-6xl  ">
